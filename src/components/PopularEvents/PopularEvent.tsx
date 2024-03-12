4600a999492cf077d10903b4357ad90fe20798ed
oscarboe <Oscarboen@gmail.com>
2024-03-12 11:06:06 +0100

    added row-reverse to the cards

--- src/components/PopularEvents/PopularEvent.tsx
+++ src/components/PopularEvents/PopularEvent.tsx
@@ -5,16 +5,17 @@ import '../Button.css';
 type PopularEventProps = {
   imageUrl: string;
   text: string;
+  swap?: boolean;
 };
 
 class PopularEvent extends React.Component<PopularEventProps> {
   render() {
     return (
-      <div className='PopularEvent'>
+      <div className='PopularEvent' style={{ flexDirection: this.props.swap ? 'row' : 'row-reverse' }}>
         <img className='img' src={this.props.imageUrl} alt='Event' />
         <div className='h1'>
           <div className='text'>{this.props.text}</div>
-          <button className='button-28' style={{ width: '50%',  }}>Se Mere</button>
+          <button className='button-28' style={{ width: '50%' }}>Se Mere</button>
         </div>
       </div>
     );
--- src/components/PopularEvents/PopularEventsWrapper.tsx
+++ src/components/PopularEvents/PopularEventsWrapper.tsx
@@ -9,14 +9,17 @@ class PopularEventsWrapper extends React.Component {
         <PopularEvent
           imageUrl='https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-12.jpg?ssl=1'
           text='Event 1 is awesome please join it'
+          swap={true}
         />
         <PopularEvent
           imageUrl='https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?auto=compress&cs=tinysrgb&w=600'
           text='Event 2 is awesome please join it'
+          swap={false}
         />
         <PopularEvent
           imageUrl='https://images.pexels.com/photos/547119/pexels-photo-547119.jpeg?auto=compress&cs=tinysrgb&w=600'
           text='Event 3 is awesome please join it'
+          swap={true}
         />
       </div>
     );
