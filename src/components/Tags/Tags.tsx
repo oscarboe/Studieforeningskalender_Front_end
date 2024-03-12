96952e5a3a46b13c74500d5047094fa5a5e725df
Lasse Haarup Jensen <lassehj00@gmail.com>
2024-03-12 10:28:11 +0100

    slider stuff

---
+++ src/components/Tags/Tags.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './Tags.scss';
+import { exampleTags } from '../../../public/ExampleData';
+
+type tagsProp = {
+  tagClicked: (tagName: string) => void;
+  searchTags: string[];
+};
+
+const Tags = ({ tagClicked, searchTags }: tagsProp) => {
+  return (
+    <div id="tags">
+      {exampleTags.map((tag, key) => (
+        <button id="tag" onClick={() => tagClicked(tag)} key={key}>
+          <p id="tagName" className={searchTags.includes(tag) ? 'isClicked' : ''}>
+            {tag}
+          </p>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Tags;
---
+++ src/pages/EventsSlider/EventsSlider.tsx
@@ -0,0 +1,64 @@
+import './EventsSlider.scss';
+import { ExampleEvents, cardInfo } from '../../../public/ExampleData';
+import EventCard from '../../components/EventCard/EventCard';
+import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
+import { useEffect, useState } from 'react';
+
+const EventsSlider = () => {
+  const [events, setEvents] = useState<cardInfo[]>([]);
+  const [currentEvents, setCurrentEvents] = useState<cardInfo[]>([]);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState<number[]>([]);
+
+  const fetchEvents = async (pageNumber: number) => {
+    // const response = await fetch data
+    // Set events data
+    const fetchedEvents = ExampleEvents.slice((pageNumber - 1) * 3, (pageNumber - 1) * 3 + 3);
+
+    if (events.length <= pageNumber * 3) setEvents([...events, ...fetchedEvents]);
+
+    setCurrentEvents(fetchedEvents);
+  };
+
+  function changePage(pageNumber: number) {
+    if (pageNumber > 0 && pageNumber <= pages.length && pageNumber != page) setPage(pageNumber);
+  }
+
+  useEffect(() => {
+    var dots = Math.ceil(ExampleEvents.length / 3);
+    for (var i = 0; i < dots; i++) {
+      setPages((pages) => [...pages, i]);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchEvents(page);
+  }, [page]);
+
+  return (
+    <div id="eventsSlider">
+      <div id="slider">
+        <button id="backButton" onClick={() => changePage(page - 1)}>
+          <SlArrowLeft />
+        </button>
+        <div id="currentEvents">
+          {currentEvents.map((event, key) => (
+            <EventCard event={event} key={key} />
+          ))}
+        </div>
+        <button id="nextButton" onClick={() => changePage(page + 1)}>
+          <SlArrowRight />
+        </button>
+      </div>
+      <div id="dots">
+        {pages.map((num, key) => (
+          <div id="dotBox" key={key}>
+            <span id="dot" className={page == key + 1 ? 'onPageDot' : ''} onClick={() => changePage(key + 1)} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventsSlider;
