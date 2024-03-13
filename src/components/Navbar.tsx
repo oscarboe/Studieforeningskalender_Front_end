f8768775a7cbb333125c410d06a05725bc0fb6c9
Lasse Haarup Jensen <lassehj00@gmail.com>
2024-03-13 14:02:05 +0100

    Assembled the home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,51 +2,37 @@ import { NavLink } from 'react-router-dom';
 import { IoCalendarOutline } from 'react-icons/io5';
 import './Navbar.css';
 import ToggleSlider from './ToggleSlider';
-import Button from './Button';
 import { useState } from 'react';
 
 const Navbar = () => {
-  const [loggedIn, setLoggedIn] = useState(false); // Add this line
-  return (
-    <header className='header'>
-      <nav className='nav container'>
-        <NavLink to='/' className='nav__logo'>
-          StudieforeningsKalenderen
-        </NavLink>
+	const [loggedIn, setLoggedIn] = useState(false); // Add this line
+	const isHome = window.location.pathname + window.location.search == '/';
 
-        <div className={'nav__menu'} id='nav-menu'>
-          <ul className='nav__list'>
-            <li className='nav__item'>
-              <NavLink to='/' className='nav__link'>
-                <ToggleSlider
-                  toggle={false}
-                  text={['Populære events', 'Det sker snart']}
-                />
-              </NavLink>
-            </li>
-            <li className='nav__item'>
-              <NavLink to='/kalender' className='nav__link'>
-                <IoCalendarOutline size='2.5em' />
-              </NavLink>
-            </li>
-            <li className='nav__item'>
-              <NavLink to='/login' className='nav__link'>
-                {loggedIn ? (
-                  <button className='button-28'>
-                    Profil
-                    </button>
-                ) : (
-                  <button className='button-28'>
-                    Login
-                    </button>                
-                    )}
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-  );
+	return (
+		<nav className={(isHome ? 'isHome ' : '') + 'nav container'}>
+			<NavLink to='/' className='nav__logo'>
+				StudieforeningsKalenderen
+			</NavLink>
+
+			<ul className='nav__list'>
+				<li className='nav__item'>
+					<NavLink to='/' className='nav__link'>
+						<ToggleSlider toggle={false} text={['Populære events', 'Det sker snart']} />
+					</NavLink>
+				</li>
+				<li className='nav__item'>
+					<NavLink to='/kalender' className='nav__link'>
+						<IoCalendarOutline size='2.5em' />
+					</NavLink>
+				</li>
+				<li className='nav__item'>
+					<NavLink to='/login' className='nav__link'>
+						{loggedIn ? <button id='profileButton'>Profil</button> : <button id='loginButton'>Login</button>}
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
--- src/components/PopularEvents/PopularEvent.tsx
+++ src/components/PopularEvents/PopularEvent.tsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import './PopularEvent.css';
-import '../Button.css';
 import { cardInfo } from '../../../public/ExampleData';
 
 type PopularEventProps = {
-  eventData: cardInfo;
-  swap?: boolean;
+	eventData: cardInfo;
+	swap?: boolean;
 };
 
 class PopularEvent extends React.Component<PopularEventProps> {
-  render() {
-    return (
-      <div
-        className='PopularEvent'
-        style={{ flexDirection: this.props.swap ? 'row' : 'row-reverse' }}
-      >
-        <img className='img' src={this.props.eventData.image} alt='Event' />
-        <div className='h1'>
-          <div className='header'>{this.props.eventData.eventName}</div>
-          <div className='description'>{this.props.eventData.description}</div>
-          <button className='button-28' style={{ width: '50%' }}>
-            Se Mere
-          </button>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='PopularEvent' style={{ flexDirection: this.props.swap ? 'row' : 'row-reverse' }}>
+				<img className='img' src={this.props.eventData.image} alt='Event' />
+				<div className='h1'>
+					<div id='textPart'>
+						<div className='title'>{this.props.eventData.eventName}</div>
+						<div className='description'>{this.props.eventData.description}</div>
+					</div>
+					<button id='eventButton'>Se Mere</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default PopularEvent;
