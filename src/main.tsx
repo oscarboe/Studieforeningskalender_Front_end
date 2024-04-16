fc06cee26dddeabce61de37e24e73a22f00da934
oscarboe <Oscarboen@gmail.com>
2024-04-16 14:50:39 +0200

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
 import './index.css';
 import ReactDOM from 'react-dom';
 import App from './App.tsx';
@@ -11,10 +12,11 @@ var uri = 'https://backend.studieforeningskalender.com';
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') uri = 'http://localhost:5022/graphql';
 
 const client = new ApolloClient({
-	link: new HttpLink({
+	link: new createUploadLink({
 		uri: uri,
 		headers: {
 			'Content-Type': 'application/json',
+			'GraphQL-Preflight': 'true',
 		},
 		credentials: 'include',
 	}),
--- src/pages/AddEventPage/AddEventPage.tsx
+++ src/pages/AddEventPage/AddEventPage.tsx
@@ -1,15 +1,68 @@
 import React, { useState } from 'react';
-import SelectedTags from '../../components/SelectedTags/SelectedTags'; // replace './SelectedTags' with the actual path to your SelectedTags.tsx file
-import Tags from '../../components/Tags/Tags';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import './AddEventPage.scss';
+import { useMutation, useQuery } from '@apollo/client';
+import {
+	CreateEventInput,
+	CreateEventMutation,
+	CreateEventMutationVariables,
+	TagsQuery,
+} from '../../../generated/graphql/graphql';
+import { GET_ALL_TAGS } from '../../Queries/TagQueries';
+import { CREATE_EVENT_QUERY } from '../../Queries/EventQueries';
 
 export default function AddEventPage() {
 	const [eventName, setEventName] = useState('');
-	const [eventDate, setEventDate] = useState('');
 	const [description, setDescription] = useState('');
+	const [startTime, setStartTime] = useState('');
+	const [endTime, setEndTime] = useState('');
+	const [imageURLs, setImageURLs] = useState<string[]>([]);
+	const [file, setFile] = useState<File | null>(null);
+
+	const { data } = useQuery<TagsQuery>(GET_ALL_TAGS);
+	const [createEvent] = useMutation<CreateEventMutation, CreateEventMutationVariables>(CREATE_EVENT_QUERY);
+	const [selectedTags, setSelectedTags] = useState<string[]>([]);
 
 	const handleSubmit = (event: React.FormEvent) => {
 		event.preventDefault();
-		console.log(`Submitting Event Name ${eventName}, Event Date ${eventDate}, Description ${description}`);
+		console.log(eventName, description, startTime, endTime, selectedTags);
+		console.log(imageURLs);
+		const input: CreateEventInput = {
+			title: eventName,
+			description: description,
+			image: file,
+			startTime: startTime,
+			endTime: endTime,
+			tags: selectedTags,
+		};
+		console.log(input);
+		createEvent({
+			variables: {
+				createEventInput: input,
+			},
+		});
+	};
+	const handleImageClick = (index: number) => {
+		setImageURLs((prevImageURLs) => {
+			const newImageURLs = [...prevImageURLs];
+			const clickedImageURL = newImageURLs.splice(index, 1)[0];
+			newImageURLs.unshift(clickedImageURL);
+			return newImageURLs;
+		});
+	};
+	const handleFileChange = (e) => {
+		if (e.target.files.length > 0) {
+			console.log(e.target.files);
+			const url = URL.createObjectURL(e.target.files[0]);
+			setFile(e.target.files[0]);
+			if (imageURLs.length < 3) setImageURLs([...imageURLs, url]);
+			else setImageURLs([...imageURLs.slice(1, 3), url]);
+			console.log(imageURLs);
+		}
 	};
 
 	return (
@@ -19,16 +72,61 @@ export default function AddEventPage() {
 				<input type='text' value={eventName} onChange={(e) => setEventName(e.target.value)} />
 			</label>
 			<label>
-				Dato:
-				<input type='date' value={eventDate} onChange={(e) => setEventDate(e.target.value)} />
+				Starttidspunkt:
+				<input type='datetime-local' value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+			</label>
+			<label>
+				Sluttidspunkt:
+				<input type='datetime-local' value={endTime} onChange={(e) => setEndTime(e.target.value)} />
 			</label>
 			<label>
 				Beskrivelse:
+				<div></div>
 				<textarea value={description} onChange={(e) => setDescription(e.target.value)} />
 			</label>
-			<SelectedTags />
-			<Tags />
-			<input type='submit' value='Submit' />
+			<div></div>
+			<label>
+				Tags:
+				<Autocomplete
+					multiple
+					id='tags-select'
+					options={data?.tags.map((tag) => ({ label: tag.name, id: tag.id })) || []}
+					getOptionLabel={(option) => option.label}
+					isOptionEqualToValue={(option, value) => option.id === value.id}
+					onChange={(newValue) => {
+						setSelectedTags(newValue);
+					}}
+					renderInput={(params) => <TextField {...params} variant='standard' label='Vælg tags' />}
+					disableCloseOnSelect={true}
+					filterSelectedOptions={true}
+				/>
+			</label>
+			<label>
+				Pictures:
+				<Tooltip title='Her kan du uploade op til 3 billeder der repræsenterer dit event. Billedet med den gyldne kant er hovedbilledet. Hvis du ønsker at et af de andre 2 billeder skal være hovedbillede så klik på det. '>
+					<IconButton>
+						<InfoIcon />
+					</IconButton>
+				</Tooltip>
+				<input type='file' accept='image/*' multiple onChange={handleFileChange} />
+			</label>
+			{imageURLs.map((url, index) => (
+				<img
+					className={`image ${index === 0 ? 'first-image' : ''}`}
+					key={index}
+					src={url}
+					alt={`Selected ${index}`}
+					onClick={() => handleImageClick(index)}
+				/>
+			))}
+			<button
+				onClick={(e) => {
+					e.preventDefault;
+					handleSubmit;
+				}}
+			>
+				Submit
+			</button>
 		</form>
 	);
 }
