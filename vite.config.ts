496d2626c0d9cd5e1ce2b1317319ed9b54892d30
oscarboe <Oscarboen@gmail.com>
2024-05-06 09:19:03 +0200

    deleted unused certificates

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+//import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
@@ -8,6 +8,6 @@ export default defineConfig(({ mode }) => {
 		define: {
 			'process.env.REACT_APP_SITE_KEY': JSON.stringify(env.REACT_APP_SITE_KEY),
 		},
-		plugins: [react(), basicSsl()],
+		plugins: [react() /*, basicSsl()*/],
 	};
 });
